a3f60db82539369b6b7f616e9cae7f80dd4a7ccc
MoloxTM <andrea.pruvostlamy@gmail.com>
2022-11-20 22:21:56 +0100

    Tâche de lecture de répertoire
    -Class DirectoryManager terminé (Accès rep et décomposition rep)
    -Class Main mise à jour

---
+++ src/fr/r34/metagg/DirectoryManager.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package fr.r34.metagg;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * @author andre
+ *
+ */
+public class DirectoryManager {
+
+	/**
+	 * Gestionnaire de répertoire permettant de reprérer et accéder aux différents dossier, sous dossiers et fichiers
+	 * @version 0.0.2
+	 * @author Andrea PL, Naïman Mpl
+	 * @return odtInFolder La liste des fichiers .odt contu dans le dossier passé en paramètre et ses sous dossiers
+	 * @throws IOException 
+	 * @throws MalformedURLException 
+	 */
+	public ArrayList<File> directoryContent(File folder){
+		try {
+			ArrayList<File> odtInFolder = new ArrayList<>();
+			for(File element : folder.listFiles()) {
+				if(element.isDirectory())
+					directoryContent(element);
+				else {
+					String mimetype = element.toURL().openConnection().getContentType();
+					if (Objects.equals(mimetype, "application/vnd.oasis.opendocument.text")) {
+						String fileName = element.getName();
+						odtInFolder.add(element);
+				
+					}
+				}
+			}
+			for(File o : odtInFolder) {
+				System.out.println(o.getName());
+			}
+			return odtInFolder;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+}
--- src/fr/r34/metagg/Main.java
+++ src/fr/r34/metagg/Main.java
@@ -1,16 +1,22 @@
 package fr.r34.metagg;
 
 import java.io.*;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
 
+    	DirectoryManager dirM = new DirectoryManager();
+    	File folder = new File("C:\\Users\\Utilisateur\\OneDrive\\Documents\\Devoir\\L2_informatique\\POO java\\ProjetJava\\ParcoursODT");
+    	ArrayList<File> odtInFolder = new ArrayList<>();
+    	odtInFolder = dirM.directoryContent(folder);
+    	
         FileManager fileM = new FileManager();
         File file = new File("expose.odt");
         File newFile = fileM.changeExtension(file, ".zip");
         fileM.readMetaData(newFile);
-
+      
     }
-
 }
