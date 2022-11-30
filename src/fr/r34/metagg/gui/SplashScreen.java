7014d2305fdd03d84631a7ac3ad9bd71165a43b5
MoloxTM <andrea.pruvostlamy@gmail.com>
2022-11-30 19:46:28 +0100

    Custom boutton et splashscreen

---
+++ src/fr/r34/metagg/gui/SplashScreen.java
@@ -0,0 +1,30 @@
+package fr.r34.metagg.gui;
+
+import javax.swing.*;
+import java.io.IOException;
+public class SplashScreen {
+
+    private ImageIcon background;
+    private JWindow window;
+    private JLabel bgContainer;
+
+    public SplashScreen(){
+        window = new JWindow();
+        background = new ImageIcon("./assets/img/splashscreen.png");
+        bgContainer = new JLabel(background);
+        window.getContentPane().add(bgContainer);
+        window.setSize(720, 514);
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        window.dispose();
+    }
+
+    public static void main(String[] arg) throws IOException {
+        new SplashScreen();
+    }
+}
