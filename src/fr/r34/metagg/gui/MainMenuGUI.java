1aa096c888d50814afb5f23e01e395e9ca5978da
Naiman <85957227+NaimanMpl@users.noreply.github.com>
2022-12-20 23:52:40 +0100

    Modification de l'ouverture d'un fichier / dossier
    La liste des fichiers ouverts se réinitalise lors de l'ouverture d'un fichier ou dossier

--- src/fr/r34/metagg/gui/MainMenuGUI.java
+++ src/fr/r34/metagg/gui/MainMenuGUI.java
@@ -127,7 +127,10 @@ public class MainMenuGUI {
             int response = fileChoose.showOpenDialog(null);
             if (response == JFileChooser.APPROVE_OPTION) {
                 File file = fileChoose.getSelectedFile();
-                if (!metaFilesOpened.isEmpty()) saveAllFiles();
+                if (!metaFilesOpened.isEmpty()) {
+                    saveAllFiles();
+                    metaFilesOpened.clear();
+                }
                 if (file.isDirectory()) {
                     // new FolderMenuGUI(main, file);
                     try {
